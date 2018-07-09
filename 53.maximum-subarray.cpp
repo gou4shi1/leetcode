cb77a911e57748b82584971b6098249d9cbe25de
诟屍 <hi@goushi.me>
2018-07-10 00:42:49 +0800

    update 53.maximum-subarray.cpp

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -30,9 +30,6 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if (nums.size() == 0)
-            return 0;
-
         int ans = nums[0], sum = 0;
         for (int num: nums) {
             sum += num;
