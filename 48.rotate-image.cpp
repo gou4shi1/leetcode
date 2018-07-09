048e5fa1e2984d0552a0ffa2662aac6bf6726bf6
诟屍 <hi@goushi.me>
2018-07-10 00:16:56 +0800

    update 48.rotate-image.cpp

---
+++ 48.rotate-image.cpp
@@ -0,0 +1,87 @@
+/*
+ * [48] Rotate Image
+ *
+ * https://leetcode.com/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (42.66%)
+ * Total Accepted:    172.8K
+ * Total Submissions: 402.5K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * You are given an n x n 2D matrix representing an image.
+ * 
+ * Rotate the image by 90 degrees (clockwise).
+ * 
+ * Note:
+ * 
+ * You have to rotate the image in-place, which means you have to modify the
+ * input 2D matrix directly. DO NOT allocate another 2D matrix and do the
+ * rotation.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Given input matrix = 
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [4,5,6],
+ * ⁠ [7,8,9]
+ * ],
+ * 
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * ⁠ [7,4,1],
+ * ⁠ [8,5,2],
+ * ⁠ [9,6,3]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Given input matrix =
+ * [
+ * ⁠ [ 5, 1, 9,11],
+ * ⁠ [ 2, 4, 8,10],
+ * ⁠ [13, 3, 6, 7],
+ * ⁠ [15,14,12,16]
+ * ], 
+ * 
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * ⁠ [15,13, 2, 5],
+ * ⁠ [14, 3, 4, 1],
+ * ⁠ [12, 6, 8, 9],
+ * ⁠ [16, 7,10,11]
+ * ]
+ * 
+ * 
+ */
+//#include "header.h"
+class Solution {
+    void swap(int &a, int &b) {
+        int t = a;
+        a = b;
+        b = t;
+    }
+
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        if (n == 0)
+            return;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+            }
+        }
+
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < n / 2; ++i) {
+                swap(matrix[i][j], matrix[n - 1 - i][j]);
+            }
+        }
+    }
+};
