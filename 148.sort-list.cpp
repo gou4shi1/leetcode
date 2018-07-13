b8ab8aaa2d88aed7c263ece8f2499d6f1370794e
诟屍 <hi@goushi.me>
2018-07-13 22:32:06 +0800

    update 148.sort-list.cpp

--- 148.sort-list.cpp
+++ 148.sort-list.cpp
@@ -36,6 +36,46 @@
 //#include "header.h"
 
 class Solution {
+    ListNode* merge(ListNode *p1, ListNode *p2) {
+        ListNode l(0);
+        ListNode *p = &l;
+
+        while (p1 && p2) {
+            if (p1->val <= p2->val) {
+                p->next = p1;
+                p1 = p1->next;
+            } else {
+                p->next = p2;
+                p2 = p2->next;
+            }
+            p = p->next;
+        }
+
+        if (p1)
+            p->next = p1;
+        else if (p2)
+            p->next = p2;
+
+        return l.next;
+    }
+
+    ListNode* mergeSort(ListNode *head) {
+        if (!head || !head->next)
+            return head;
+
+        ListNode *fp = head, *sp = head, *pre_sp;
+        while (fp && fp->next) {
+            pre_sp = sp;
+            sp = sp->next;
+            fp = fp->next->next;
+        }
+        pre_sp->next = nullptr;
+        
+        ListNode *left = mergeSort(head);
+        ListNode *right = mergeSort(sp);
+        return merge(left, right);
+    }
+
     void quickSort(ListNode *head, ListNode *tail) {
         if (!head || head == tail)
             return;
@@ -67,6 +107,8 @@ public:
     ListNode* sortList(ListNode* head) {
         if (!head || !head->next)
             return head;
+
+        return mergeSort(head);
 
         ListNode *tail = head;
         while (tail->next)
