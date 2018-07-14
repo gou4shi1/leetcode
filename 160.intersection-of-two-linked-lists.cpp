c08dcf197beacc6e178c366522f8db64ffb3aad7
诟屍 <hi@goushi.me>
2018-07-14 23:09:45 +0800

    update 160.intersection-of-two-linked-lists.cpp

---
+++ 160.intersection-of-two-linked-lists.cpp
@@ -0,0 +1,75 @@
+/*
+ * [160] Intersection of Two Linked Lists
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+ *
+ * algorithms
+ * Easy (30.80%)
+ * Total Accepted:    206.7K
+ * Total Submissions: 672.8K
+ * Testcase Example:  'No intersection: []\n[]'
+ *
+ * Write a program to find the node at which the intersection of two singly
+ * linked lists begins.
+ * 
+ * For example, the following two linked lists: 
+ * 
+ * A:          a1 → a2
+ * ⁠                  ↘
+ * ⁠                    c1 → c2 → c3
+ * ⁠                  ↗            
+ * B:     b1 → b2 → b3
+ * 
+ * begin to intersect at node c1.
+ * 
+ * Notes:
+ * 
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function
+ * returns. 
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ * 
+ * 
+ * 
+ * Credits:Special thanks to @stellari for adding this problem and creating all
+ * test cases.
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//#include "header.h"
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p = headA, *q = headB, *pp = headA, *qq = headB;
+        while (p && q) {
+            p = p->next;
+            q = q->next;
+        }
+        if (p) {
+            while (p) {
+                pp = pp->next;
+                p = p->next;
+            }
+        }
+        if (q) {
+            while (q) {
+                qq = qq->next;
+                q = q->next;
+            }
+        }
+        while (pp && qq && pp != qq) {
+            pp = pp->next;
+            qq = qq->next;
+        }
+
+        return pp == qq ? pp : nullptr;
+    }
+};
