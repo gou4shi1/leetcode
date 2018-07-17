8305908e2dd0c0d58c9d7bd33f21bc07e4013186
诟屍 <hi@goushi.me>
2018-07-17 14:09:28 +0800

    update 647.palindromic-substrings.cpp

---
+++ 647.palindromic-substrings.cpp
@@ -0,0 +1,70 @@
+/*
+ * [647] Palindromic Substrings
+ *
+ * https://leetcode.com/problems/palindromic-substrings/description/
+ *
+ * algorithms
+ * Medium (54.41%)
+ * Total Accepted:    48.7K
+ * Total Submissions: 89.6K
+ * Testcase Example:  '"abc"'
+ *
+ * 
+ * Given a string, your task is to count how many palindromic substrings in
+ * this string.
+ * 
+ * 
+ * 
+ * The substrings with different start indexes or end indexes are counted as
+ * different substrings even they consist of same characters. 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The input string length won't exceed 1000.
+ * 
+ * 
+ */
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int len = s.length();
+        if (!len)
+            return 0;
+        
+        vector<int> pre(1, 0);
+        int ans = 1;
+        for (int i = 1; i < len; ++i) {
+            vector<int> curr(1, i);
+            ++ans;
+            if (s[i] == s[i - 1]) {
+                curr.push_back(i - 1);
+                ++ans;
+            }
+            for (int j: pre) {
+                if (j > 0 && s[j - 1] == s[i]) {
+                    curr.push_back(j - 1);
+                    ++ans;
+                }
+            }
+            pre = curr;
+        }
+        return ans;
+    }
+};
