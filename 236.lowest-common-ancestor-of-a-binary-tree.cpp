72d87df8bd3207ec6f6a54327b25df222ae966f6
诟屍 <hi@goushi.me>
2018-07-17 22:24:50 +0800

    update 236.lowest-common-ancestor-of-a-binary-tree.cpp

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,87 @@
+/*
+ * [236] Lowest Common Ancestor of a Binary Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (30.54%)
+ * Total Accepted:    178.4K
+ * Total Submissions: 584.1K
+ * Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given
+ * nodes in the tree.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * 
+ * 
+ * ⁠       _______3______
+ * ⁠      /              \
+ * ⁠   ___5__          ___1__
+ * ⁠  /      \        /      \
+ * ⁠  6      _2       0       8
+ * ⁠        /  \
+ * ⁠        7   4
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of of nodes 5 and 1 is 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant
+ * of itself
+ * ⁠            according to the LCA definition.
+ * 
+ * Note:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the binary tree.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+//#include "header.h"
+
+class Solution {
+    // if find both, return LCA
+    TreeNode* findOneOf(TreeNode *r, TreeNode *p, TreeNode *q) {
+        if (!r)
+            return nullptr;
+
+        if (r == p || r == q)
+            return r;
+
+        TreeNode *left = findOneOf(r->left, p, q);
+        TreeNode *right = findOneOf(r->right, p, q);
+        if (left && right)
+            return r;
+        return left ? left : right;
+    }
+
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return findOneOf(root, p, q);
+    }
+};
